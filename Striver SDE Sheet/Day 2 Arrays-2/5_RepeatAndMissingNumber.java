f8d149e954457a140dacd9e95ba53e3882d12156
AdityaShidlyali <adityamshidlyali@gmail.com>
2023-02-02 12:12:31 +0530

    Added one more approach for Repeat and Missing Number

--- Striver SDE Sheet/Day 2 Arrays-2/5_RepeatAndMissingNumber.java
+++ Striver SDE Sheet/Day 2 Arrays-2/5_RepeatAndMissingNumber.java
@@ -26,3 +26,50 @@ public class Solution {
         return ansList;
     }
 }
+
+// Approach 2: Using XOR
+class Solve {
+    int[] findTwoElement(int a[], int n) {
+        int xorTillN = 0;
+        int xorArrayNums = 0;
+        
+        for (int i : a) {
+            xorArrayNums ^= i;
+        }
+        
+        for (int i = 1; i <= n; i++) {
+            xorTillN ^= i;
+        }
+        
+        int xorFinal = xorTillN ^ xorArrayNums;
+        
+        int rightMostSetBit = xorFinal & ~(xorFinal - 1);
+        
+        int x = 0, y = 0;
+        for (int i = 0; i < n; i++) {
+            if ((a[i] & rightMostSetBit) != 0)
+                x = x ^ a[i];
+            else
+                y = y ^ a[i];
+        }
+        
+        for (int i = 1; i <= n; i++) {
+            if ((i & rightMostSetBit) != 0)
+                x = x ^ i;
+            else
+                y = y ^ i;
+        }
+        
+        int xCount = 0;
+        for (int i = 0; i < n; i++) {
+            if (a[i] == x)
+                xCount++;
+        }
+        
+        if (xCount==0) {
+            return new int[]{y, x};
+        } else {
+            return new int[]{x, y};
+        }
+    }
+}
