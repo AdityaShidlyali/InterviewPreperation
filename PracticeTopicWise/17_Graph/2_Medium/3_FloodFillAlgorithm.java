ccd478271bb7ff30f0ea28ec1fdc6f02a90ce042
AdityaShidlyali <adityamshidlyali@gmail.com>
2023-03-06 22:48:39 +0530

    Flood Fill Algorithm update

--- PracticeTopicWise/17_Graph/2_Medium/3_FloodFillAlgorithm.java
+++ PracticeTopicWise/17_Graph/2_Medium/3_FloodFillAlgorithm.java
@@ -18,7 +18,7 @@ class Solution {
 	private static boolean isPositionValid(int[][] image, int row, int col, int newColor, int oldColor) {
 		if (row < image.length && row >= 0
 		&& col < image[0].length && col >= 0
-// 		&& !visited[row][col
+// 		&& !visited[row][col]
 		&& image[row][col] != newColor
 		&& image[row][col] == oldColor) {
 			return true;
@@ -122,6 +122,54 @@ class Solution {
 		boolean[][] visited = new boolean[m][n];
 		
 		bfs(image, sr, sc, newColor, oldColor, visited);
+		return image;
+	}
+}
+
+// OR
+
+class  Solution {
+
+	private static int[] rowPositions = new int[] {-1, 0, 1, 0};
+	private static int[] colPositions = new int[] {0, 1, 0, -1};
+	
+	private static boolean isPositionValid(int[][] image, int row, int col, int newColor, int oldColor, boolean[][] visited) {
+		if (row < image.length && row >= 0
+		&& col < image[0].length && col >= 0
+        && !visited[row][col]
+		&& image[row][col] != newColor
+		&& image[row][col] == oldColor
+        ) {
+			return true;
+		}
+		
+		return false;
+	}
+
+    private static void dfs(int[][] image, int i, int j, int newColor, int oldColor, boolean[][] visited) {
+        image[i][j] = newColor;
+        visited[i][j] = true;
+
+        for (int pos = 0; pos < 4; pos++) {
+            int neighborRow = i + rowPositions[pos];
+            int neighborCol = j + colPositions[pos];
+
+            if (isPositionValid(image, neighborRow, neighborCol, newColor, oldColor, visited)) {
+                dfs(image, neighborRow, neighborCol, newColor, oldColor, visited);
+            }
+        }
+    }
+
+	public int[][] floodFill(int[][] image, int sr, int sc, int newColor) {
+		int m = image.length;
+		int n = image[0].length;
+		int oldColor = image[sr][sc];
+		boolean[][] visited = new boolean[m][n];
+
+        image[sr][sc] = newColor;
+        visited[sr][sc] = true;
+		
+		dfs(image, sr, sc, newColor, oldColor, visited);
 		return image;
 	}
 }
